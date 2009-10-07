a5a84b2bcf9c32f533d9e96ac0dd051ff6670086
F. Visconte <f.visconte@gmail.com>
2009-10-07 19:25:57 +0200

    more simple

--- chips/src/bContourFinder.h
+++ chips/src/bContourFinder.h
@@ -15,46 +15,17 @@ typedef vector<ofPoint> bHullShape;
 class bContourFinder : public ofxCvContourFinder {
   
   public: 
-
-    //vector<ofxCvBlob>  blobs;
-    //int                nBlobs;    // DEPRECATED: use blobs.size() instead
- 
-
     bContourFinder() : ofxCvContourFinder() {}; 
     
-    virtual int  findContours( ofxCvGrayscaleImage& input,
-                               int minarea, int maxarea,
-                               int nconsidered, bool bfindholes,
-                               bool buseapproximation = true){
-        int i = ofxCvContourFinder::findContours(input, minarea, maxarea, nconsidered, bfindholes, buseapproximation);
-        return i;
-
-    }
-                               // approximation = don't do points for all points 
-                               // of the contour, if the contour runs
-                               // along a straight line, for example...
-
-
+    void findConvexHulls();
     virtual void  draw() { draw(0,0, _width, _height); };
     virtual void  draw( float x, float y ) { draw(x,y, _width, _height); };
-    virtual void  draw( float x, float y, float w, float h ){ ofxCvContourFinder::draw(x, y, w, h);  }; 
-	virtual void setAnchorPercent(float xPct, float yPct){ ofxCvContourFinder::setAnchorPercent(xPct, yPct); }
-    virtual void setAnchorPoint(int x, int y) { ofxCvContourFinder::setAnchorPoint(x, y); };
-	virtual void resetAnchor(){ofxCvContourFinder::resetAnchor(); };      
-    
     void reset();
-    
-
-    int findConvexHulls(ofxCvGrayscaleImage&  input,
-									  int minArea,
-									  int maxArea,
-									  int nConsidered,
-									  bool bFindHoles,
-                                      bool bUseApproximation);
-
-
+    virtual void  draw( float x, float y, float w, float h ); //{ ofxCvContourFinder::draw(x, y, w, h);  }; 
+  
     vector <CvSeq *> convexHulls;
     vector <bHullShape> hulls;
+    vector <ofxCvBlob> convexBlobs; 
 
 };
 
